cb3cc6c48b74e9281d3609d96a0f8c3c2ebf84f0
ForestSquirrel <Vasiliy.14vasiliy@yandex.ru>
2020-12-22 18:56:02 +0300

    Finished project, some improvements may be needed

--- NavalBattle/Source.cpp
+++ NavalBattle/Source.cpp
@@ -1,6 +1,193 @@
-#include "DiscordGameClient.h"
 
+#include "API/sleepy-discord/include/sleepy_discord/websocketpp_websocket.h"
+#include "API/sleepy-discord/include/sleepy_discord/sleepy_discord.h"
+#include "API/sleepy-discord/include/sleepy_discord/embed.h"
+
+#include "NavalBattle.h"
+
+
+class NavalBattleClient : public SleepyDiscord::DiscordClient {
+private:
+    GameHandler GH;
+public:
+    using SleepyDiscord::DiscordClient::DiscordClient;
+    SleepyDiscord::Embed embedHelp;
+    SleepyDiscord::EmbedField fieldHelp;
+    bool Turn = true; //true - first player, false - second player
+
+    void onMessage(SleepyDiscord::Message message) {
+        //
+        if (message.startsWith("NB debug")) {
+            sendMessage(message.channelID, " debug msg");
+        }
+        if (message.startsWith("NB help")) {
+            embedHelp.title = "Naval assistant 2.0";
+            embedHelp.fields.clear();
+            fieldHelp.isInline = true;
+            fieldHelp.name = "Help for game";
+            fieldHelp.value += ship + " represents ship \n";
+            fieldHelp.value += water + " represents water \n";
+            fieldHelp.value += Dship + " represents damaged/destroyed ship \n";
+            fieldHelp.value += miss + " miss \n";
+            fieldHelp.value += "Commands \n";
+            fieldHelp.value += "NB reg - registers player for game \n";
+            fieldHelp.value += "NB shoot <arg> - allows you to shoot: usage NB shoot a1 \n";
+            fieldHelp.value += "NB show opp - show your opponents field \n";
+            fieldHelp.value += "NB show me - show your field \n";
+            embedHelp.fields.push_back(fieldHelp);
+
+            sendMessage(message.channelID, "", embedHelp);
+        }
+        //register player
+        if (message.startsWith("NB reg")) {
+            if (message.author.username == GH.getFirstPlayerName() || message.author.username == GH.getSecondPlayerName()) {
+                sendMessage(message.channelID, message.author.username + " You are already in game");
+            }
+            else if (GH.getFirstPlayerName().empty()) {
+                GH.setfirstPlayerName(message.author.username);
+                sendMessage(message.channelID, message.author.username + " Successfully reged for game, waiting second player");
+            }
+            else if (GH.getSecondPlayerName().empty()) {
+                GH.setSecondPlayerName(message.author.username);
+                sendMessage(message.channelID, message.author.username + " Successfully reged for game, starting game");
+            }
+            else {
+                sendMessage(message.channelID, "Game is full already");
+                return;
+            }
+        }
+        //shoot command
+        if (message.startsWith("NB shoot")) {
+            if (message.author.username != GH.getFirstPlayerName() && message.author.username != GH.getSecondPlayerName()) {
+                sendMessage(message.channelID, message.author.username + " You are not player");
+                return;
+            }
+            //parse message content
+            std::string tile;
+            std::string msg, msgField;
+            for (auto i = 9; i < message.content.size(); i++) {    // i = 10 because starts with ...
+                tile += message.content.at(i);
+            }
+
+            if (message.author.username == GH.getFirstPlayerName() && Turn) {
+                try {
+                    GH.shoot(GH.getFirstPlayerName(), tile);
+                    //msgField = GH.getBlankFieldByPlayer(GH.getFirstPlayerName(), true);
+                    msgField = ASCIIartField(GH.getBlankFieldByPlayerMatrix(GH.getFirstPlayerName(), true));
+                    if (!GH.isMiss(IdToIndex(tile), GH.getFirstPlayerName())) {
+                        msg = "Shoot again your aim is great\n";
+                    }
+                    else {
+                        msg = "Miss!!!\n";
+                        Turn = false;
+                    }
+                    if (GH.getFirstPlayerPoints() == 20) {
+                        msg = "You won! Congratulations";
+                        sendMessage(message.channelID, msg);
+                        GH.clear();
+                    }
+                    else {
+                        sendMessage(message.channelID, msg + msgField /*,embedTemp*/);
+                    }
+                }
+                catch (std::invalid_argument& e) {
+                    sendMessage(message.channelID, message.author.username + " " + e.what());
+                }
+            }
+            else if (message.author.username == GH.getSecondPlayerName() && !Turn) {
+                try {
+                    GH.shoot(GH.getSecondPlayerName(), tile);
+                    //msgField = GH.getBlankFieldByPlayer(GH.getSecondPlayerName(), true);
+                    msgField = ASCIIartField(GH.getBlankFieldByPlayerMatrix(GH.getSecondPlayerName(), true));
+                    if (!GH.isMiss(IdToIndex(tile), GH.getSecondPlayerName())) {
+                        msg = "Shoot again your aim is great\n";
+                    }
+                    else {
+                        msg = "Miss!!!\n";
+                        Turn = true;
+                    }
+                    if (GH.getSecondPlayerPoints() == 20) {
+                        msg = "You won! Congratulations";
+                        sendMessage(message.channelID, msg);
+                        GH.clear();
+                    }
+                    else {
+                        sendMessage(message.channelID, msg + msgField/*, embedTemp*/);
+                    }
+                }
+                catch (std::invalid_argument& e) {
+                    sendMessage(message.channelID, message.author.username + " " + e.what());
+                }
+            }
+            else {
+                sendMessage(message.channelID, message.author.username + " It's not your turn yet");
+            }
+        }
+
+        //show opponents field
+        if (message.startsWith("NB show opp")) {
+            if (message.author.username != GH.getFirstPlayerName() && message.author.username != GH.getSecondPlayerName()) {
+                sendMessage(message.channelID, message.author.username + " You are not player");
+                return;
+            }
+            std::string msg = "";
+
+            if (message.author.username == GH.getFirstPlayerName()) {
+                try {
+                    //msg = GH.getBlankFieldByPlayer(GH.getFirstPlayerName(), true);
+                    msg = ASCIIartField(GH.getBlankFieldByPlayerMatrix(GH.getFirstPlayerName(), true));
+                    sendMessage(message.channelID, msg);
+                }
+                catch (std::invalid_argument& e) {
+                    sendMessage(message.channelID, message.author.username + " " + e.what());
+                }
+            }
+            else if (message.author.username == GH.getSecondPlayerName()) {
+                try {
+                   //msg = GH.getBlankFieldByPlayer(GH.getSecondPlayerName(), true);
+                   msg = ASCIIartField(GH.getBlankFieldByPlayerMatrix(GH.getSecondPlayerName(), true));
+                   sendMessage(message.channelID, msg);
+                }
+                catch (std::invalid_argument& e) {
+                    sendMessage(message.channelID, message.author.username + " " + e.what());
+                }
+            }
+        }
+
+        //show my field
+        if (message.startsWith("NB show me")) {
+            if (message.author.username != GH.getFirstPlayerName() && message.author.username != GH.getSecondPlayerName()) {
+                sendMessage(message.channelID, message.author.username + " You are not player");
+                return;
+            }
+            std::string msg = "";
+
+            if (message.author.username == GH.getFirstPlayerName()) {
+                try {
+                    //msg = GH.getBlankFieldByPlayer(GH.getFirstPlayerName(), false);
+                    msg = ASCIIartField(GH.getBlankFieldByPlayerMatrix(GH.getFirstPlayerName(), false));
+                    sendMessage(message.channelID, msg/*, embedTemp*/);
+                }
+                catch (std::invalid_argument& e) {
+                    sendMessage(message.channelID, message.author.username + " " + e.what());
+                }
+            }
+            else if (message.author.username == GH.getSecondPlayerName()) {
+                try {
+                    //msg = GH.getBlankFieldByPlayer(GH.getSecondPlayerName(), false);
+                    msg = ASCIIartField(GH.getBlankFieldByPlayerMatrix(GH.getSecondPlayerName(), false));
+                    sendMessage(message.channelID, msg);
+                }
+                catch (std::invalid_argument& e) {
+                    sendMessage(message.channelID, message.author.username + " " + e.what());
+                }
+            }
+        }
+
+
+    }
+};
 int main() {
-    NavalBattleClient client("NzQzNDkyMTQ3ODg5ODMxOTg4.XzVdBQ.v-YEPck2on7eQh4rRLKTjHwuH1M", 2);
+    NavalBattleClient client("NzQzNDkyMTQ3ODg5ODMxOTg4.XzVdBQ.LlCuBhNmdB1YtwB0w-RmMP0HHHk", 2);
     client.run();
 }
--- NavalBattle/utility.h
+++ NavalBattle/utility.h
@@ -4,13 +4,142 @@
 #include <string>
 #include <stdexcept>
 #include <algorithm>
+#include <string_view>
 
 //this file has some utility functions for better handling
 //discord api. Since we can only work with messages we need 
 //some parsing and askii art to make our bot user-friendly
 
+//utility stuff for gameHandler
+enum class Title {
+	EMPTY,
+	SHIP,
+	BORDER,
+	HIT,
+	MISS,
+	DESTROYED_SHIP
+};
 
+enum class Ship {
+	//including borders!
+	Large = 6,
+	Big = 5,
+	Medium = 4,
+	Small = 3
+};
 
+enum class Player {
+	Human,
+	Opponent
+};
+
+const int
+FIELD_SIZE = 12,
+FIELD_SIZE_WITHOUT_BORDERS = 10,
+OFFSET = 1,
+BORDER_OFFSET = 3;
+
+//Discord is kinda tricky thing when it comes to making UI
+//since we cant realy use images to show users what is  happening
+//Here comes the solution ASCII ART
+//It may sound strange, but discord supports embeds and that's
+//there ASCII art comes in handy
+
+//ASCII art constants 
+const std::string line =	"+===+===+===+===+===+===+===+===+===+===+===+\n";
+const std::string letters = "|   | A | B | C | D | E | F | G | H | I | J |\n";
+const std::string A =		"| 1 |";
+const std::string B =		"| 2 |";
+const std::string C =		"| 3 |";
+const std::string D =		"| 4 |";
+const std::string E =		"| 5 |";
+const std::string F =		"| 6 |";
+const std::string G =		"| 7 |";
+const std::string H =		"| 8 |";
+const std::string I =		"| 9 |";
+const std::string J =		"| 10|";
+const std::string ship =	"###";
+const std::string water =	"~~~";
+const std::string miss =	"<o>";
+const std::string Dship =	">#<"; //destroyed ship or damaged ship
+const std::string SP =		"|"; //spacer
+
+std::string ASCIIartField(Title (*matrix)[FIELD_SIZE]) {
+	std::string art;
+	auto currField = matrix;
+	//header
+	art += "`";
+	art += line;
+	art += letters;
+	art += line;
+	for (int y = 1; y < FIELD_SIZE - 1; ++y) {
+		switch (y) {
+		case 1:
+			art += A;
+			break;
+		case 2:
+			art += B;
+			break;
+		case 3:
+			art += C;
+			break;
+		case 4:
+			art += D;
+			break;
+		case 5:
+			art += E;
+			break;
+		case 6:
+			art += F;
+			break;
+		case 7:
+			art += G;
+			break;
+		case 8:
+			art += H;
+			break;
+		case 9:
+			art += I;
+			break;
+		case 10:
+			art += J;
+			break;
+		}
+		for (int x = 1; x < FIELD_SIZE - 1; ++x) {
+			switch (currField[y][x]) {
+			case Title::EMPTY:
+				art += water;
+				art += SP;
+				break;
+			case Title::SHIP:
+				art += ship;
+				art += SP;
+				break;
+			case Title::BORDER:
+				art += water;
+				art += SP;
+				break;
+			case Title::HIT:
+				art += Dship;
+				art += SP;
+				break;
+			case Title::MISS:
+				art += miss;
+				art += SP;
+				break;
+			case Title::DESTROYED_SHIP:
+				art += Dship;
+				art += SP;
+				break;
+			}
+		}
+		art += "\n";
+		art += line;
+	}
+	art += "`";
+	return art;
+}
+//input parsing
 std::pair<int, int> IdToIndex(std::string& Id) { // this function converts string to pair e.g. a1 -> <0, 0> or g8 -> <6, 7>
 	if (Id.empty() || Id.size() > 3) {
 		throw std::invalid_argument("Wrong index");
